848a47df5d09707835e4cab1f31dba3dfab99100
Davy Josias Scheuermann <93287491+Davyjosias01@users.noreply.github.com>
2025-01-09 11:12:08 -0300

    Configurando o factory bot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,7 @@
 require 'spec_helper'
+require 'vcr'
+require 'simplecov'
+
 
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
@@ -13,15 +16,27 @@ begin
 rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
-RSpec.configure do |config|
 
+
+RSpec.configure do |config|
   config.fixture_paths = [
     Rails.root.join('spec/fixtures')
-  ]
-
-
+  ] 
   config.use_transactional_fixtures = true
-
   config.filter_rails_from_backtrace!
-
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'fixtures/vcr_cassettes'
+  config.hook_into :webmock
+end
+
+SimpleCov.start 
+
